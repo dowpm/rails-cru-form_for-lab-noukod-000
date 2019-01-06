51fdf4aeadc1d30521d08014d193d986c6ee1b3c
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-06 16:04:25 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,34 @@
 class SongsController < ApplicationController
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.create post_params(:name, :bio)
+    redirect_to song_path(@song)
+  end
+
+  def show
+    set_song
+  end
+
+  def edit
+    set_song
+  end
+
+  def update
+    set_song
+    @song.update post_params(:name, :bio)
+    redirect_to song_path(@song)
+  end
+
+  private
+
+  def set_song
+    @song = Song.find(params[:id])
+  end
+
+  def post_params *arg
+    params.require(:song).permit(*arg)
+  end
 end
